655130c95414360a1d19dee6f0b9964ad947ac2a
Sorfinaly <sorfinaxoxo27@gmail.com>
2024-01-13 13:43:18 +0800

    update

--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,7 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*", // Match any network id
-      from:"0x8437a8eaed08A9Ef56A24dBc73449f9929F43011"
+      from:"0xAff235C0f435120c435E92C8BaA74fdEBE6E4F4D"
     },
     develop: {
       port: 8545
